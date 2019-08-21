bee865ac560082f04e81694eda567d9b96e9cdec
Mantis <mantis@Connors-MacBook-Pro.local>
2019-08-21 14:11:06 -0700

    changes

---
+++ server.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let path = require("path");
+let app = express();
+
+var PORT = process.env.PORT || 6464;
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  
+  // Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
